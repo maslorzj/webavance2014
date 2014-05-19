559f8bd14e906bcd2eefc06f80b88d36cd077580
wtragnee <w.tragnee@gmail.com>
2014-05-19 17:54:02 +0200

    angular suite

--- test_angular/test.js
+++ test_angular/test.js
@@ -2,14 +2,39 @@ myApp.controller('TestController', ['$scope', '$http', 'TestService', function($
 	$scope.TestService = TestService;
 	$scope.search = {};
 	$scope.searchType = 'title';
-	$scope.orders = ['title', 'editor'];
+	$scope.filterOrders = ['editor', 'authorName', 'title'];
+	$scope.filters = {editor:null};
+	$scope.currentOrder = 'editor';
+	$scope.currentPrinted = {};
+
 
 	$scope.resetSearchField = function() 
 	{
 		$scope.search = {};
 	};
 
-	$scope.test = function(input) {
-		console.log(input);
+	$scope.nextOrder = function(filter) {
+		var currentIndex = 0;
+		angular.forEach($scope.filterOrders, function(order, index){
+			if(order == $scope.currentOrder) {
+				currentIndex = index;
+			}
+		});
+		$scope.filters[$scope.currentOrder] = filter;
+		if(currentIndex < $scope.filterOrders.length - 1) {
+			$scope.currentOrder = $scope.filterOrders[currentIndex+1];
+		}
 	};
+
+	$scope.filterTree = function() {
+    	return function(bd) {
+    		var bool = true;
+    		angular.forEach($scope.filters, function(value, filter){
+	    		if(filter !== "$$hashKey" && value !== null && bd[filter] != value) {
+		      		bool = false;
+	    		}
+    		});
+    		return bool;
+    	};
+  	};
 }]);
--- test_angular/testService.js
+++ test_angular/testService.js
@@ -1,9 +1,9 @@
 myApp.service('TestService', function(){
 	var BDs = [];
-	BDs.push({isbn:1234,editor:'truc',title:"John",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
-	BDs.push({isbn:1235,editor:'machin',title:"John",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
-	BDs.push({isbn:1236,editor:'chouette',title:"Jack",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
-	BDs.push({isbn:1237,editor:'choses',title:"Jack",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1234,editor:'editeur1',title:"titre1",authorName:'name1',authorFirstame:'firstname1',illusName:'ilname1',illusFirstname:'ilfirstname1',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1235,editor:'editeur1',title:"titre2",authorName:'name1',authorFirstame:'firstname2',illusName:'ilname2',illusFirstname:'ilfirstname2',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1236,editor:'editeur1',title:"titre3",authorName:'name3',authorFirstame:'firstname3',illusName:'ilname3',illusFirstname:'ilfirstname3',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1237,editor:'editeur4',title:"titre4",authorName:'name4',authorFirstame:'firstname4',illusName:'ilname4',illusFirstname:'ilfirstname4',language:'French',publishDate:'On verra après'});
 
 	this.BDs = BDs;
 });
