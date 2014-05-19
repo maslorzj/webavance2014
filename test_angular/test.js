32d7b5a308c216c2773071ede68d5432586f70f2
wtragnee <w.tragnee@gmail.com>
2014-05-19 15:35:56 +0200

    angular suite

--- test_angular/test.js
+++ test_angular/test.js
@@ -1,3 +1,15 @@
 myApp.controller('TestController', ['$scope', '$http', 'TestService', function($scope, $http, TestService){
 	$scope.TestService = TestService;
+	$scope.search = {};
+	$scope.searchType = 'title';
+	$scope.orders = ['title', 'editor'];
+
+	$scope.resetSearchField = function() 
+	{
+		$scope.search = {};
+	};
+
+	$scope.test = function(input) {
+		console.log(input);
+	};
 }]);
--- test_angular/testService.js
+++ test_angular/testService.js
@@ -1,7 +1,9 @@
 myApp.service('TestService', function(){
-	this.BDs = [];
-	this.BDs.push({ISBN:1234,title:"Il faut voir les...",saga:"Troie"});
-	this.BDs.push({ISBN:1235,title:"... choses en face...",saga:"Troie"});
-	this.BDs.push({ISBN:1236,title:"... je suis...",saga:"Troie"});
-	this.BDs.push({ISBN:1237,title:"... une patate !",saga:"Troie"});
+	var BDs = [];
+	BDs.push({isbn:1234,editor:'truc',title:"John",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1235,editor:'machin',title:"John",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1236,editor:'chouette',title:"Jack",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
+	BDs.push({isbn:1237,editor:'choses',title:"Jack",authorName:'Bon',authorFirstame:'Jean',illusName:'Bono',illusFirstname:'Jean',language:'French',publishDate:'On verra après'});
+
+	this.BDs = BDs;
 });
