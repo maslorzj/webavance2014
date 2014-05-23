52ff618bc926de3aebaba6ac3ee807ed0e5a2e56
wtragnee <w.tragnee@gmail.com>
2014-05-23 17:01:29 +0200

    angular

--- test_angular/test.js
+++ test_angular/test.js
@@ -1,12 +1,16 @@
 myApp.controller('TestController', ['$scope', '$http', 'TestService', function($scope, $http, TestService){
-	$scope.TestService = TestService;
-	$scope.search = {};
-	$scope.searchType = 'title';
-	$scope.filterOrders = ['editor', 'authorName', 'title'];
-	$scope.filters = {editor:null};
-	$scope.currentOrder = 'editor';
-	$scope.currentPrinted = {};
 
+	$scope.initController = function() {
+		$scope.filterOrders = ['editor', 'authorName', 'title'];
+		$scope.filters = {editor:null};
+		$scope.currentOrder = 0;
+		$scope.currentPrinted = {};
+		$scope.bds = TestService.bds;
+		$scope.maxTableSize = 12;
+		$scope.getBds($scope.bds);
+		$scope.getBdsForTree();
+		$scope.getBdsForTable();
+	};
 
 	$scope.resetSearchField = function() 
 	{
@@ -14,27 +18,48 @@ myApp.controller('TestController', ['$scope', '$http', 'TestService', function($
 	};
 
 	$scope.nextOrder = function(filter) {
-		var currentIndex = 0;
-		angular.forEach($scope.filterOrders, function(order, index){
-			if(order == $scope.currentOrder) {
-				currentIndex = index;
-			}
+		$scope.filters[$scope.filterOrders[$scope.currentOrder]] = filter;
+		if($scope.currentOrder < $scope.filterOrders.length-1) {
+			$scope.currentOrder++;
+		}
+		$scope.getBds($scope.bds);
+	};
+
+	$scope.lastOrder = function() {
+		$scope.currentOrder--;
+		delete($scope.filters[$scope.filterOrders[$scope.currentOrder]]);
+		$scope.getBds(TestService.bds);
+	};
+
+	$scope.getBds = function() {
+		$scope.currentPrinted = [];
+		angular.forEach($scope.currentPrinted, function(value, key){
+			$scope.currentPrinted.push(value);
 		});
-		$scope.filters[$scope.currentOrder] = filter;
-		if(currentIndex < $scope.filterOrders.length - 1) {
-			$scope.currentOrder = $scope.filterOrders[currentIndex+1];
+	};
+
+	$scope.getBds = function(bdList) {
+		if($scope.currentOrder < $scope.filterOrders.length) {
+			$scope.bds = [];
+			$scope.currentPrinted = {};
+			angular.forEach(bdList, function(bd){
+				var bool = true;
+				angular.forEach($scope.filters, function(value, key){
+					if(key != "$$hashKey" && value != null && bd[key] != value) {
+						bool = false;
+					}
+				});
+				if (bool) {
+					if($scope.currentPrinted[bd[$scope.filterOrders[$scope.currentOrder]]] == null){
+						$scope.currentPrinted[bd[$scope.filterOrders[$scope.currentOrder]]] = bd[$scope.filterOrders[$scope.currentOrder]];
+					}
+					$scope.bds.push(bd);
+				};
+			});
 		}
 	};
 
-	$scope.filterTree = function() {
-    	return function(bd) {
-    		var bool = true;
-    		angular.forEach($scope.filters, function(value, filter){
-	    		if(filter !== "$$hashKey" && value !== null && bd[filter] != value) {
-		      		bool = false;
-	    		}
-    		});
-    		return bool;
-    	};
-  	};
+	$scope.test = function(input) {
+		console.log(input);
+	};
 }]);
