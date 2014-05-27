16f7b0f0bdaa3978e947ba194d41f3867ac0e88d
wtragnee <w.tragnee@gmail.com>
2014-05-27 14:58:39 +0200

    formulaire angualr

--- test_angular/addBd.js
+++ test_angular/addBd.js
@@ -5,21 +5,42 @@ myApp.controller('AddBDController', ['$scope', '$http', function($scope, $http){
 	};
 
 	$scope.addField = function() {
-		if ($scope.newBd.personnalFields.length == 0) {
-			$scope.newBd.personnalFields.push({});
-		} else {
-			var lastLine = $scope.newBd.personnalFields[$scope.newBd.personnalFields.length-1];
-			if(lastLine.key != null && lastLine.value != null && lastLine.key != "" && lastLine.value != "") {
+		if($scope.newBd.personnalFields.length < 5) {
+			if ($scope.newBd.personnalFields.length == 0) {
 				$scope.newBd.personnalFields.push({});
-			}
+			} else {
+				var lastLine = $scope.newBd.personnalFields[$scope.newBd.personnalFields.length-1];
+				if(lastLine.key && lastLine.value) {
+					$scope.newBd.personnalFields.push({});
+				} else {
+					$scope.newFieldError = "You must fill the value of the last fields to continue."
+				}
+			}	
+		} else {
+			$scope.newFieldError = "You can not have more than 5 personnal fields."
 		}
 	};
 
 	$scope.deleteField = function(index) {
   		$scope.newBd.personnalFields.splice(index, 1);
+  		$scope.newFieldError = null;
 	};
 
+	$scope.submitKeyUp = function() {
+		if(event.keyCode == 13){
+			$scope.submit();
+      	}
+	}
+
 	$scope.submit = function() {
-		console.log($scope.newBd);	
+		$http({
+		    url: "add",
+		    method: "POST",
+		    data: {newBd:JSON.stringify($scope.newBd)}
+		}).success(function(data, status, headers, config) {
+		    $scope.data = data;
+		}).error(function(data, status, headers, config) {
+		    $scope.status = status;
+		});
 	};
 }]);
